0b6469eaa4f66cffa3b66d380a0ac54b504f8480
Ajay Prajapat <ajayprajapat@live.com>
2022-09-03 11:43:24 +0530

    fixed

--- nest-quiz/src/app.module.ts
+++ nest-quiz/src/app.module.ts
@@ -1,23 +1,23 @@
 import { QuestionSchema } from './@schemas/question.schema';
-import { UserService } from "./user/user.service";
-import { UserController } from "./user/user.controller";
-import { UserSchema } from "./@schemas/user.schema";
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TopicSchema } from "./@schemas/topic.schema";
-import { SummarySchema } from "./@schemas/summary.schema";
-import { SubjectSchema } from "./@schemas/subject.schema";
-import { QuizSchema } from "./@schemas/quiz.schema";
-import { PracticeResultSchema } from "./@schemas/practice-result.schema";
-import { LiveResultSchema } from "./@schemas/live-result.schema";
-import { SummaryController } from "./summary/summary.controller";
-import { SummaryService } from "./summary/summary.service";
-import { SubjectController } from "./subject/subject.controller";
-import { SubjectService } from "./subject/subject.service";
-import { QuizController } from "./quiz/quiz.controller";
-import { QuizService } from "./quiz/quiz.service";
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserSchema } from './@schemas/user.schema';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TopicSchema } from './@schemas/topic.schema';
+import { SummarySchema } from './@schemas/summary.schema';
+import { SubjectSchema } from './@schemas/subject.schema';
+import { QuizSchema } from './@schemas/quiz.schema';
+import { PracticeResultSchema } from './@schemas/practice-result.schema';
+import { LiveResultSchema } from './@schemas/live-result.schema';
+import { SummaryController } from './summary/summary.controller';
+import { SummaryService } from './summary/summary.service';
+import { SubjectController } from './subject/subject.controller';
+import { SubjectService } from './subject/subject.service';
+import { QuizController } from './quiz/quiz.controller';
+import { QuizService } from './quiz/quiz.service';
 import { ResultController } from './result/result.controller';
 import { ResultService } from './result/result.service';
 import { QuestionController } from './question/question.controller';
@@ -25,16 +25,18 @@ import { QuestionService } from './question/question.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb+srv://ajayprajapat:bvqtJJLgnG2sS8dE@cluster0.toa6t.mongodb.net/quiz"),
+    MongooseModule.forRoot(
+      'mongodb+srv://ajayprajapat:bvqtJJLgnG2sS8dE@cluster0.toa6t.mongodb.net/quiz',
+    ),
     MongooseModule.forFeature([
-      { name: "User", schema: UserSchema },
-      { name: "Topic", schema: TopicSchema },
-      { name: "Summary", schema: SummarySchema },
-      { name: "Subject", schema: SubjectSchema },
-      { name: "Quiz", schema: QuizSchema },
-      { name: "Practice-Result", schema: PracticeResultSchema },
-      { name: "Live-Result", schema: LiveResultSchema },
-      { name: "Question", schema: QuestionSchema },
+      { name: 'User', schema: UserSchema },
+      { name: 'Topic', schema: TopicSchema },
+      { name: 'Summary', schema: SummarySchema },
+      { name: 'Subject', schema: SubjectSchema },
+      { name: 'Quiz', schema: QuizSchema },
+      { name: 'Practice-Result', schema: PracticeResultSchema },
+      { name: 'Live-Result', schema: LiveResultSchema },
+      { name: 'Question', schema: QuestionSchema },
     ]),
   ],
   controllers: [
@@ -56,4 +58,4 @@ import { QuestionService } from './question/question.service';
     QuestionService,
   ],
 })
-export class AppModule { }
+export class AppModule {}
