c723d8e6fbe4c490c5479b25a59737324d4c3f29
Ajay Prajapat <ajayprajapat@live.com>
2022-09-03 11:29:03 +0530

    admin added

---
+++ equiz-admin/src/app/pages/contact/contact.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSort } from "@angular/material/sort";
+import { ContactService } from "src/app/services/contact.service";
+
+@Component({
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"]
+})
+export class ContactComponent implements OnInit {
+  displayedColumns: string[] = ["number", "email", "subject", "key"];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  isLoading = false;
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.contactService.getAll().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+
+      setTimeout(() => {
+        this.dataSource.filterPredicate = (
+          data: { name: string },
+          filterValue: string
+        ) =>
+          data.name
+            .trim()
+            .toLowerCase()
+            .indexOf(filterValue) !== -1;
+
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }, 100);
+
+      this.isLoading = true;
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
---
+++ equiz-admin/src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDaSgLQjpwXzR85myM9C9uZLzfSoLFO9IE",
+    authDomain: "quiz-20818.firebaseapp.com",
+    projectId: "quiz-20818",
+    storageBucket: "quiz-20818.appspot.com",
+    messagingSenderId: "810373445913",
+    appId: "1:810373445913:web:0d15a4b80792ab83326d88",
+    measurementId: "G-G7S3KTE6G8",
+    databaseURL: "https://quiz-20818-default-rtdb.firebaseio.com/",
+  },
+  defaultAnswerCount: 4, //Answer input count,
+  snackBarConfig: {
+    duration: 2 * 1000
+  },
+  ONESIGNAL_APP_ID: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  ONESIGNAL_REST_API_KEY: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+};
